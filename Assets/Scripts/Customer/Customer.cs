84371b66146aced35b29b0a16c8c8149c0fcca91
Jiayao Yu <yjybxx2@yahoo.com>
2025-01-25 14:51:04 +0100

    Switched from OpenXR to OculusXR backend.

--- Assets/Scripts/Customer/Customer.cs
+++ Assets/Scripts/Customer/Customer.cs
@@ -1,49 +1,113 @@
+using System.Collections;
 using UnityEngine;
 
 public class Customer : MonoBehaviour
 {
-    [SerializeField] private Transform customerTransform;
-    [SerializeField] private float activeDuration = 10f;
+    [SerializeField] private Transform _customerTransform;
+    // [SerializeField] private Customers _customersController;
+    [SerializeField] private float _activeDurationEasy = 10f;
+    [SerializeField] private float _activeDurationMedium = 7f;
+    [SerializeField] private float _activeDurationHard = 4f;
+    [SerializeField] private float _inactiveDuration = 5f;
 
     private bool _isActive = false;
-    private float _timer = 0f;
-
-    public void Activate()
-    {
-        _isActive = true;
-    }
+    private float _activeTimer = 0f;
+    private float _inactiveTimer = 0f;
+    private float _activeDuration; // how long time a customer is active in one turn
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _activeDuration = _activeDurationEasy;
+
+        StartCoroutine(ShowCustomer());
+    }
+
+    IEnumerator ShowCustomer()
+    {
+        yield return new WaitForSeconds(_inactiveDuration);
         _isActive = true;
         PresentCustomer();
     }
 
+    IEnumerator UnshowCustomer()
+    {
+        yield return new WaitForSeconds(_activeDuration);
+        
+        Debug.Log("UnshowCustomer();");
+        _activeTimer = 0f;
+        _isActive = false;
+        HideCustomer();
+    }
+
     // Update is called once per frame
+    /*
     void Update()
     {
-        if (_isActive)
+        SetCustomerDifficulty(_customersController.gameDifficulty);
+        Debug.Log("deltaTime: " +  Time.deltaTime);
+
+        if (!_isActive)
         {
-            _timer += Time.deltaTime;
+            _inactiveTimer += Time.deltaTime;
+            Debug.Log("_inactiveTimer: " + _inactiveTimer);
         }
 
-        if (_timer >= activeDuration)
+        if (_inactiveTimer >= _inactiveDuration)
         {
-            _timer = 0f;
+            Debug.Log("Present customer");
+            _isActive = true;
+            PresentCustomer();
+        }
+        
+        if (_isActive)
+        {
+            _activeTimer += Time.deltaTime;
+        }
+
+        if (_activeTimer >= _activeDuration)
+        {
+            _activeTimer = 0f;
             _isActive = false;
             HideCustomer();
         }
-    }
 
+        // DeactivationCheck();
+        // ActivationCheck();
+    }
+    */
+    
+
+    private void SetCustomerDifficulty(GameDifficulty gameDifficulty)
+    {
+        switch (gameDifficulty)
+        {
+            case GameDifficulty.Easy:
+                _activeDuration = _activeDurationEasy;
+                break;
+            case GameDifficulty.Medium:
+                _activeDuration = _activeDurationMedium;
+                break;
+            case GameDifficulty.Hard:
+                _activeDuration = _activeDurationHard;
+                break;
+            default:
+                break;
+        }
+        Debug.Log("set difficulty: " + gameDifficulty);
+    }
+    
     private void PresentCustomer()
     {
-        customerTransform.eulerAngles -= new Vector3(0f, 0f, 90f);
+        Debug.Log("PresentCustomer();");
+        _customerTransform.eulerAngles -= new Vector3(0f, 0f, 90f);
+
+        StartCoroutine(UnshowCustomer());
     }
 
     private void HideCustomer()
     {
-        customerTransform.eulerAngles += new Vector3(0f, 0f, 90f);
+        _customerTransform.eulerAngles += new Vector3(0f, 0f, 90f);
     }
 }
--- Assets/Scripts/Customer/Customers.cs
+++ Assets/Scripts/Customer/Customers.cs
@@ -1,16 +1,43 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+public enum GameDifficulty
+{
+    Easy = 0,
+    Medium = 1,
+    Hard = 2,
+}
 
 public class Customers : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float gameDuration { private get; set; }
+    public GameDifficulty gameDifficulty { get; private set; }
+
+    [SerializeField] private List<Customer> _customers;
+
+    
+    private float _timer;
+    
     void Start()
     {
-        
+        if (gameDuration == 0) { gameDuration = 60f; }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _timer += Time.deltaTime;
+        if (_timer >= gameDuration * 2 / 3)
+        {
+            gameDifficulty = GameDifficulty.Hard;
+        }
+        else if (_timer >= gameDuration / 3)
+        {
+            gameDifficulty = GameDifficulty.Medium;
+        }
+        else
+        {
+            gameDifficulty = GameDifficulty.Easy;
+        }
     }
 }
