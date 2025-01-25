d2a757e3c9624dd2b6edaa8c4f6ae3032e7881e6
Jiayao Yu <yjybxx2@yahoo.com>
2025-01-25 11:29:21 +0100

    Drafty beer tap prefab. I'll abort my work since Bogdan is working on the same thing.

---
+++ Assets/Scripts/BeerTap/BeerTapController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class BeerTapController : MonoBehaviour
+{
+    [SerializeField] private GameObject _lever;
+    [SerializeField] private GameObject _tap;
+    
+    
+}
